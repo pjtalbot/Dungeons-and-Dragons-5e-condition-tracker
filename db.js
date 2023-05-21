3c72412dfa8a26437bb852d34577f414559d5915
pauljordantalbot <pauljordantalbot@gmail.com>
2023-05-21 15:29:30 -0400

    DB_URI changed for deployment

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ let DB_URI;
 
 if (process.env.NODE_ENV === 'test') {
 	DB_URI = 'postgresql:///roller_test';
+}
+if (process.env.NODE_ENV === 'production') {
+	DB_URI = process.env.DATABASE_URL;
 } else {
 	DB_URI = 'postgresql:///roller';
 }
